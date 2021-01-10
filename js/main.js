afa066ae2e106fd3fd721dd7ef04e03ffc760e57
A. Nazaew <abdoelmalik.nazaew@hva.nl>
2021-01-10 18:56:05 +0100

    updated videocall

--- js/main.js
+++ js/main.js
@@ -79,5 +79,15 @@ $(window).on('load', function() {
 	    bottomSpacing: 60
 	});
 
+	var video = document.getElementById('video');
 
+	// Get access to the camera!
+	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+		// Not adding `{ audio: true }` since we only want video now
+		navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+			//video.src = window.URL.createObjectURL(stream);
+			video.srcObject = stream;
+			video.play();
+		});
+	}
 })(jQuery);
